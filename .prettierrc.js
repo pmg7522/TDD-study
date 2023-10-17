2d5f04081c00d6fe75f8c48a825fa2a06d45cf65
pmg7522 <pmg752290@gmail.com>
2023-10-17 18:54:32 +0900

    feat: 서버 기본 세팅

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    semi: true,
-    bracketSpacing: true,
-    singleQuote: false,
-    trailingComma: "es5",
-    arrowParens: "avoid",
-    printWidth: 130,
-    tabWidth: 4,
-};
