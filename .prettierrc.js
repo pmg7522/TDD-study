611b39c5b896e5c336aba34fb355180583426016
pmg7522 <pmg752290@gmail.com>
2023-10-17 18:47:41 +0900

    feat: eslint, prettier, tsconfig 설정

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    bracketSpacing: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    arrowParens: "avoid",
+    printWidth: 130,
+    tabWidth: 4,
+};
