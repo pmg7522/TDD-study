2d8a804315c459aef2bf9769369312051bbf1140
pmg7522 <pmg752290@gmail.com>
2023-11-07 16:40:07 +0900

    feat: typeORM 설치 및 데이터베이스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,25 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from '@/common/middlewares/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CatsModule } from './apis/cats/cats.module';
 
 @Module({
-    imports: [CatsModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            synchronize: true,
+        }),
+        CatsModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer.apply(LoggerMiddleware).forRoutes('*');
-    }
-}
+export class AppModule {}
