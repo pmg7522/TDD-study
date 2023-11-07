ceca029b966b2ea4e8af68b5897e253c85f4583f
pmg7522 <pmg752290@gmail.com>
2023-11-07 17:01:46 +0900

    feat: class-validation 작동하도록 작성

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const { PORT } = process.env;
 
     const app = await NestFactory.create(AppModule);
 
+    app.useGlobalPipes(new ValidationPipe());
     app.useGlobalFilters(new HttpExceptionFilter());
 
     await app.listen(PORT);
