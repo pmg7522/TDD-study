30c93e319b107a8ab6f9f315e719d6b0f919549d
pmg7522 <pmg752290@gmail.com>
2023-11-07 16:44:16 +0900

    modify: 서버 포트 env를 이용하도록 수정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
+    const { PORT } = process.env;
+
     const app = await NestFactory.create(AppModule);
 
-    await app.listen(3000);
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    await app.listen(PORT);
 }
 bootstrap();
