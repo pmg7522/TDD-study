2eb429f2a27c4a5d1cd6d6f95a4c41132db0a7ea
pmg7522 <pmg752290@gmail.com>
2023-11-07 11:51:15 +0900

    feat: Exception Filter 공부중

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, UseFilters } from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from '@/errors/exception-filters/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
     constructor(private readonly catsService: CatsService) {}
 
     @Get()
+    @UseFilters(HttpExceptionFilter)
     getDetail(): object {
+        throw new HttpException(
+            { resultCode: 401000, message: 'unauthorized' },
+            401,
+        );
+
         return this.catsService.getDetail();
     }
 }
---
+++ src/errors/exception-filters/http-exception.filter.ts
@@ -0,0 +1,29 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+
+        const res = ctx.getResponse<Response>();
+
+        const status = exception.getStatus();
+        const error = exception.getResponse() as {
+            resultCode: number;
+            message: string;
+        };
+        console.log(exception.message);
+
+        res.status(status).json({
+            resultCode: error.resultCode,
+            message: error.message,
+            data: null,
+        });
+    }
+}
