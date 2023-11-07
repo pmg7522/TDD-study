c941cacfe66262ce3bbc0157fb6db64a374f362d
pmg7522 <pmg752290@gmail.com>
2023-11-07 10:47:40 +0900

    feat: Middleware 공부

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService, CatsService],
+    imports: [CatsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
