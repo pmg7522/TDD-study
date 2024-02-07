a8dfde14db5d8c7be4cccbd3a024b4363e187ec0
pmg7522 <pmg752290@gmail.com>
2024-02-07 19:24:34 +0900

    feat: main파일 설정 완료

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { setupSwagger } from "./common/utils/configs/swagger.config";
+import { ValidationPipe } from "@nestjs/common";
+import { SuccessInterceptor } from "./common/utils/interceptors/success.interceptor";
+import { HttpExceptionFilter } from "./common/utils/exceptions/http-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
+    /** CORS */
+    app.enableCors({ origin: true });
+
+    /** Swagger */
+    setupSwagger(app);
+
+    /** Pipe */
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            stopAtFirstError: true,
+            transformOptions: { enableImplicitConversion: true },
+        }),
+    );
+
+    /** Exception Filter */
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    /** Interceptor */
+    app.useGlobalInterceptors(new SuccessInterceptor());
+
+    /** Server */
+    await app.listen(configService.get<number>("SERVER_PORT"));
 }
 bootstrap();
