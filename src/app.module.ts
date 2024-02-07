ba4efeffa92440ccb4b8c74a19c2d0ce3b486287
pmg7522 <pmg752290@gmail.com>
2024-02-07 19:27:06 +0900

    feat: Logger Middleware 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./apis/auth/auth.module";
 import { CartModule } from "./apis/cart/cart.module";
 import { UserModule } from "./apis/user/user.module";
@@ -7,19 +8,26 @@ import { ProductModule } from "./apis/product/product.module";
 import { ProductCategoryModule } from "./apis/product/product-category/product-category.module";
 import { OrderModule } from "./apis/order/order.module";
 import { OrderDetailModule } from "./apis/order/order-detail/order-detail.module";
-import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmConfig } from "./common/utils/configs/typeorm.config";
+import { LoggerMiddleware } from "./common/utils/middlewares/logger.middleware";
 
 @Module({
     imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        UserModule,
-        ReviewModule,
-        ProductModule,
-        ProductCategoryModule,
+        ConfigModule.forRoot({ cache: true, isGlobal: true }),
+        TypeOrmModule.forRootAsync(TypeOrmConfig),
+        AuthModule,
+        CartModule,
         OrderModule,
         OrderDetailModule,
-        CartModule,
-        AuthModule,
+        ProductModule,
+        ProductCategoryModule,
+        ReviewModule,
+        UserModule,
     ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): any {
+        consumer.apply(LoggerMiddleware).forRoutes("*");
+    }
+}
